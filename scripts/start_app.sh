eb639a3082f15ffa22509bad87977e03aad6b405
Abirami <abisubash29@gmail.com>
2023-04-26 15:01:02 +0530

    commit

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/bash 
 cd /home/ubuntu/Code
 source /home/ubuntu/env/bin/activate
-export FLASK_APP=main.py
-export FLASK_ENV=production
-export SERVER_NAME=localhost:5000
-gunicorn app:app
+nohup python main.py > /home/ubuntu/logs.txt 2>&1 &
